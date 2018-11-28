a4fcbc46fd9cdd7134bc75c6ed915c406cd1f350
masthanreddy <mstnreddy@gmail.com>
2018-11-28 12:51:47 -0700

    pal-tracker checkin

---
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -0,0 +1,19 @@
+package io.pivotal.pal.tracker;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+    @GetMapping("/")
+    public String sayHello() {
+        return "hello";
+    }
+
+    @GetMapping("/hai")
+    public String sayHai() {
+
+        return "Hi PAL Team";
+    }
+}
